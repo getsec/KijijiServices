9984e2985d31051d046da50a911a629f051c7df8
nathan getty <nathan.getty@skipthedishes.ca>
2020-04-06 08:52:32 -0500

    backend/bot: updated logging

--- backend/bot.py
+++ backend/bot.py
@@ -10,7 +10,6 @@ from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import TimeoutException
 
 from huepy import good, bad, info
-from geopy.geocoders import Nominatim
 import logging
 import time
 import sys
@@ -24,11 +23,6 @@ logging.basicConfig(
     datefmt='%H:%M:%S',
     level=logging.INFO)
 
-
-def getgeo(city, province):
-    geolocator = Nominatim(user_agent="specify_your_app_name_here")
-    location = geolocator.geocode(f"{city}, {province}")
-    return location.latitude, location.longitude
 
 
 def parse_ad(ad_file):
@@ -57,13 +51,15 @@ def get_url_by_city(city):
 
 
 def validate(html, url, bot):
+    sec = "VALIDATE"
     good_message = "You have successfully posted your ad!"
     if good_message in html:
-        logging.info("Ad was created succesfully")
-        logging.info(f"Ad URL: {url}")
+        logging.info(f"{sec} - Ad was created succesfully")
+        logging.info(f"{sec} - Ad URL: {url}")
         print(good("Ad was posted succesfully"))
         print(info(f"Ad URL: {url}"))
     else:
+        logging.info(f"{sec} - Failed posting ad, dump file on server.")
         print(bad("There was an error posting the ad. Dumping HTML file."))
         dump_html(html)
         bot.get_screenshot_as_file('output.png')
@@ -90,12 +86,11 @@ def handle_region(city, prov, bot):
         EC.element_to_be_clickable(
             (By.XPATH, '//*[@id="downshift-2-input"]'))).send_keys(Keys.ENTER)
 
-    print("Looking for apply button")
     time.sleep(3)
     button_elements = bot.find_elements_by_class_name('submitButton-2124651659')
     for i in button_elements:
         if "Apply" in i.text:
-            print("Clicking {i.text} button")
+            print(f"Clicking {i.text} button")
             i.click()
 
 
@@ -103,7 +98,7 @@ def handle_region(city, prov, bot):
 class Bot:
     def __init__(self, **kwargs):
         self.headless = kwargs.get("headless", True)
-
+        sec = "INITIALIZATION"
         # Setup profile
         firefox_profile = webdriver.FirefoxProfile()
         firefox_profile.set_preference("geo.enabled", False);
@@ -114,12 +109,15 @@ class Bot:
             options = Options()
             options.headless = self.headless
             self.bot = webdriver.Firefox(firefox_profile=firefox_profile, options=options)
+            logging.info(f"{sec} - Headless: {self.headless}")
             print(info(f"Bot Initialized headless"))
         else:
             self.bot = webdriver.Firefox(firefox_profile=firefox_profile)
             print(info("Bot initialized."))
+            logging.info(f"{sec} - complete")
 
     def login(self, username, password):
+        sec = "LOGIN"
         bot = self.bot
         self.username = username
         self.password = password
@@ -133,9 +131,10 @@ class Bot:
         # If we're logged in, the text "Register should not be visible"
         assert "Register" not in bot.page_source, "Failed to login."
         print(good(f"Succesfully logged into {self.username}"))
-        logging.info(f"We're logged in to: {self.username}")
+        logging.info(f"{sec} - We're logged in to: {self.username}")
 
     def create_service_post(self, **kwargs):
+        sec = "POST_SERVICE"
         bot = self.bot
         self.ad_title = kwargs.get("ad_title")
         self.ad_desc = kwargs.get("ad_desc")
@@ -144,7 +143,7 @@ class Bot:
         self.photo = kwargs.get("photo")
         self.province = kwargs.get("province")
         self.city = kwargs.get("city")
-        logging.info("Trying to create services post witht he following arguments.")
+        logging.info(f"{sec} - Attempting to create ad with parameters")
         logging.info(kwargs)
         time.sleep(3)
         print(info(f"Clicking the right buttons to post an ad in {self.city}, {self.province}"))
@@ -155,24 +154,25 @@ class Bot:
         bot.get(self.ad_url)
 
         time.sleep(8)
-        logging.info("Waited for some time, and now we're going to post an ad.")
+        logging.info(f"{sec} - Injecting data into ad form for '{self.username}'")
         # Check to see if we get in coprrectly
         assert 'p-post-ad.html' in bot.current_url
 
-        logging.info(f"[{self.ad_title}] Trying to send data to ad title for {self.username}")
+        logging.info(f"{sec} - Sending ad title")
 
         # enter ad title
         WebDriverWait(bot, 15).until(
             EC.element_to_be_clickable(
                 (By.XPATH, '//*[@id="postad-title"]'))).send_keys(self.ad_title)
-        logging.info(f"[{self.ad_title}] Trying to send data to ad description for {self.username}")
+        
+        logging.info(f"{sec} Sending data to ad description for '{self.username}'")
 
         # ad desc
         WebDriverWait(bot, 10).until(
             EC.element_to_be_clickable(
                 (By.XPATH, '//*[@id="pstad-descrptn"]'))).send_keys(self.ad_desc)
 
-        logging.info(f"[{self.ad_title}] Trying to send data to ad address for {self.username}")
+        logging.info(f"{sec} Trying to send data to ad address for {self.username}")
         # ad address
         WebDriverWait(bot, 10).until(
             EC.element_to_be_clickable(
@@ -186,13 +186,13 @@ class Bot:
                 evt.preventDefault();
             }, true)
             """)
-        logging.info(f"[{self.ad_title}] Trying to send data to ad photo for {self.username}")
+        logging.info(f"{sec} Trying to send data to ad photo for {self.username}")
         elem = bot.find_element_by_css_selector('input[type=file]').send_keys(self.photo)
 
         time.sleep(15)  # Wait for file to upload
 
         # Since the package section has a few popups, we need to use the execute script on the elem
-        logging.info(f"[{self.ad_title}] We're clicking on the basic package for {self.username}")
+        logging.info(f"{sec} We're clicking on the basic package for {self.username}")
         element = WebDriverWait(bot, 20).until(
             EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="MainForm"]/div[6]/div/div/div/div[1]/div[1]/div[1]/div/button')))
@@ -200,47 +200,49 @@ class Bot:
 
         time.sleep(1)
 
-        logging.info(f"[{self.ad_title}] We're clicking on the submit button for {self.username}.")
+        logging.info(f"{sec} We're clicking on the submit button for {self.username}.")
         WebDriverWait(bot, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="MainForm"]/div[10]/button[1]'))).click()
-        print(info(f"[{self.ad_title}] Ad has been submitted, running validation."))
+        print(info(f"{sec} Ad has been submitted, running validation."))
         # run validate to parse the html source
         validate(bot.page_source, bot.current_url, bot)
 
     def nuke_ads(self):
+        sec = "DELETE_ADS"
         bot = self.bot
         bot.get('https://www.kijiji.ca/m-my-ads/active')
-        logging.info(f"Checking active ads for {self.username}")
+        logging.info(f"{sec} Checking active ads for {self.username}")
 
         try:
             elem = bot.find_element_by_xpath('/html/body/div[3]/div[4]/div/div/div/div[4]/ul')
             for i in elem.find_elements_by_css_selector('li'):
                 if i.text == "Delete":
                     i.click()
-                    logging.info(f"Deleting an active ad for {self.username}")
+                    logging.info(f"{sec} Deleting an active ad for {self.username}")
                     print(info("Deleting an active ad."))
                     time.sleep(3)
                 else:
-                    logging.info(f"No active ads to delete for {self.username}")
+                    logging.info(f"{sec} No active ads to delete for {self.username}")
         except Exception:
             pass
 
         bot.get('https://www.kijiji.ca/m-my-ads/inactive')
-        logging.info(f"Checking inactive ads for {self.username}")
+        logging.info(f"{sec} Checking inactive ads for {self.username}")
         try:
             elem = bot.find_element_by_xpath('/html/body/div[3]/div[4]/div/div/div/div[4]/ul')
             for i in elem.find_elements_by_css_selector('li'):
                 if i.text == "Delete":
                     i.click()
-                    logging.info(f"Deleting an inactive ad for {self.username}")
+                    logging.info(f"{sec} Deleting an inactive ad for {self.username}")
                     print(info("Deleting an inactive ad."))
                     time.sleep(3)
                 else:
-                    logging.info(f"No inactive ads to delete for {self.username}")
+                    logging.info(f"{sec} No inactive ads to delete for {self.username}")
         except Exception:
             pass
 
     def teardown(self):
+        sec = "TEARDOWN"
         bot = self.bot
-        logging.info(f"Killing the webdriver for session.")
+        logging.info(f"{sec} Killing the webdriver for session.")
         bot.quit()
-        logging.info(f"The webdriver has been killed.")
+        logging.info(f"{sec} The webdriver has been killed.")
